8c1d3aea9e4456088cf565d9cdee703fcaefc804
y3tu <14643092@qq.com>
2018-06-16 15:22:51 +0800

    Initial commit

---
+++ src/main/java/com/y3tu/y3tutool/Y3tuToolApplication.java
@@ -0,0 +1,12 @@
+package com.y3tu.y3tutool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Y3tuToolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Y3tuToolApplication.class, args);
+    }
+}
---
+++ src/test/java/com/y3tu/y3tutool/Y3tuToolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.y3tu.y3tutool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Y3tuToolApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
