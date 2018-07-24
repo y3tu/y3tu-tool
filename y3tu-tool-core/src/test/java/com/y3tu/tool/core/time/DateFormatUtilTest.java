a62a4378e194d4c072713a465e1d826a14ff368c
y3tu <14643092@qq.com>
2018-07-24 17:21:13 +0800

    更新

---
+++ y3tu-tool-core/src/test/java/com/y3tu/tool/core/time/DateFormatUtilTest.java
@@ -0,0 +1,23 @@
+package com.y3tu.tool.core.time;
+
+import com.y3tu.tool.core.lang.Console;
+import org.junit.Test;
+
+import java.text.ParseException;
+import java.util.Date;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author y3tu
+ * @date 2018/7/24
+ */
+public class DateFormatUtilTest {
+
+    @Test
+    public void formatFriendlyTimeSpanByNow() throws ParseException {
+        Date date = DateFormatUtil.DEFAULT_FORMAT.parse("2018-07-23 23:29:59.500");
+        Console.log(DateFormatUtil.formatFriendlyTimeSpanByNow(date));
+
+    }
+}
