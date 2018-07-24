964268636c05889425c507db848148eb6cced360
y3tu <14643092@qq.com>
2018-07-24 22:41:33 +0800

    更新

---
+++ y3tu-tool-core/src/test/java/com/y3tu/tool/core/util/PageUtilTest.java
@@ -0,0 +1,16 @@
+package com.y3tu.tool.core.util;
+
+import com.y3tu.tool.core.lang.Console;
+import com.y3tu.tool.core.text.ObjectPrintUtil;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class PageUtilTest {
+
+    @Test
+    public void rainbow() {
+        int[] a = PageUtil.rainbow(1,10);
+        Console.log(ObjectPrintUtil.toPrettyString(a));
+    }
+}
