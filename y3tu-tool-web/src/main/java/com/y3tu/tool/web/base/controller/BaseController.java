3f27885d8cf2bf36ccc48b80722e7f1f300355ba
y3tu <14643092@qq.com>
2018-08-05 03:08:46 +0800

    更新

--- y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/controller/BaseController.java
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/controller/BaseController.java
@@ -1,12 +1,110 @@
 package com.y3tu.tool.web.base.controller;
 
 
+import com.y3tu.tool.web.base.entity.BaseEntity;
+import com.y3tu.tool.web.base.pojo.PageInfo;
+import com.y3tu.tool.web.base.pojo.Query;
+import com.y3tu.tool.web.base.pojo.R;
+import com.y3tu.tool.web.base.service.BaseService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Controller基类
  *
  * @author y3tu
  */
-public class BaseController {
+public class BaseController<T extends BaseService,M extends BaseEntity>{
+
+    @Autowired
+    T service;
+    /**
+     * 分页查询
+     *
+     * @param params 查询参数 包含分页信息和查询条件
+     *               前台传入参数params封装规则
+     *               page：当前页
+     *               limit：每页条数
+     *               order：排序
+     *               查询条件的key要和mapper.xml文件里面的key保持一致
+     *
+     * @return
+     */
+    @RequestMapping(value = "/getByPage",method = RequestMethod.GET)
+    public R getByPage(@RequestParam Map<String, Object> params) {
+        PageInfo pageInfo = new PageInfo(service.queryPage(new Query<M>(params).getPage(), params));
+        return R.ok(pageInfo);
+    }
+
+    /**
+     * 获取全部数据
+     * @return
+     */
+    @RequestMapping(value = "/getAll",method = RequestMethod.GET)
+    public R getAll(){
+        return R.ok(service.selectList(null));
+    }
+
+    /**
+     * 通过id获取
+     * @param id
+     * @return
+     */
+    @RequestMapping(value = "/get/{id}",method = RequestMethod.GET)
+    public R get(@PathVariable String id){
+        return R.ok(service.selectById(id));
+    }
+
+    /**
+     * 保存数据
+     * @param entity 保存的数据
+     * @return
+     */
+    @RequestMapping(value = "/save",method = RequestMethod.POST)
+    public R save(@RequestParam M entity){
+        service.insert(entity);
+        return R.ok("保存成功!");
+    }
+
+    /**
+     * 更新数据
+     * @param entity 更新的数据
+     * @return
+     */
+    @RequestMapping(value = "/update",method = RequestMethod.PUT)
+    public R update(@RequestParam M entity){
+        service.updateById(entity);
+        return R.ok("更新成功!");
+    }
+
+    /**
+     * 删除
+     * @param id 主键
+     * @return
+     */
+    @RequestMapping(value = "/delById/{id}",method = RequestMethod.DELETE)
+    public R delById(@PathVariable String id){
+        service.deleteById(id);
+        return R.ok("删除成功!");
+    }
+
+    /**
+     * 批量删除
+     * @param ids 主键集合
+     * @return
+     */
+    @RequestMapping(value = "/delByIds",method = RequestMethod.DELETE)
+    public R delByIds(@RequestParam List<String> ids){
+        service.deleteBatchIds(ids);
+        return R.ok("删除成功!");
+    }
+
     /**
      * 重定向至地址 url
      *
@@ -18,8 +116,6 @@ public class BaseController {
         rto.append(url);
         return rto.toString();
     }
-
-
 
 
 }
