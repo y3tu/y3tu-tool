7b566e9e3db524bf5f32d4c967cb8df7b9120e37
y3tu <14643092@qq.com>
2018-08-06 23:48:23 +0800

    更新

---
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/aspect/RedisAspect.java
@@ -0,0 +1,40 @@
+package com.y3tu.tool.web.aspect;
+
+
+import com.y3tu.tool.web.exception.RException;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+/**
+ * Redis切面处理类
+ *
+ * @author chenshun
+ * @email sunlightcs@gmail.com
+ * @date 2017-07-17 23:30
+ */
+@Aspect
+@Component
+@Slf4j
+public class RedisAspect {
+    //是否开启redis缓存  true开启   false关闭
+    @Value("${y3tu-tool.redis.open: false}")
+    private boolean open;
+
+    @Around("execution(* com.y3tu.tool.web.util.RedisUtils.*(..))")
+    public Object around(ProceedingJoinPoint point) throws Throwable {
+        Object result = null;
+        if(open){
+            try{
+                result = point.proceed();
+            }catch (Exception e){
+                log.error("redis error", e);
+                throw new RException("Redis服务异常");
+            }
+        }
+        return result;
+    }
+}
