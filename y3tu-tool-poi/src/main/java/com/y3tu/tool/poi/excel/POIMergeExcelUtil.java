8c4144c85280723087eb5204ac0a6b4ac7cf17b7
y3tu <14643092@qq.com>
2018-08-31 18:48:27 +0800

    更新

---
+++ y3tu-tool-poi/src/main/java/com/y3tu/tool/poi/excel/POIMergeExcelUtil.java
@@ -0,0 +1,9 @@
+package com.y3tu.tool.poi.excel;
+
+/**
+ * excel合并工具类
+ * @author y3tu
+ * @date 2018/8/9
+ */
+public class POIMergeExcelUtil {
+}
---
+++ y3tu-tool-web/src/main/test/java/com/y3tu/tool/web/util/JasyptUtilTest.java
@@ -0,0 +1,18 @@
+package com.y3tu.tool.web.util;
+
+import com.y3tu.tool.core.lang.Console;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * @author y3tu
+ * @date 2018/8/31
+ */
+public class JasyptUtilTest {
+
+    @Test
+    public void encyptPwd() {
+        Console.log(JasyptUtil.encyptPwd("123456",""));
+    }
+}
