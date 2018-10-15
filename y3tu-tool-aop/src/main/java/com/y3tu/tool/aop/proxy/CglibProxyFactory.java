44f120e94a3d59c404d9c583b962139aab3ab3df
y3tu <14643092@qq.com>
2018-10-15 20:12:19 +0800

    动态代理

---
+++ y3tu-tool-aop/src/main/java/com/y3tu/tool/aop/proxy/CglibProxyFactory.java
@@ -0,0 +1,25 @@
+package com.y3tu.tool.aop.proxy;
+
+
+import com.y3tu.tool.aop.aspects.Aspect;
+import com.y3tu.tool.aop.interceptor.CglibInterceptor;
+import net.sf.cglib.proxy.Enhancer;
+
+/**
+ * 基于Cglib的切面代理工厂
+ * 
+ * @author looly
+ *
+ */
+public class CglibProxyFactory extends ProxyFactory{
+
+	@Override
+	@SuppressWarnings("unchecked")
+	public <T> T proxy(T target, Aspect aspect) {
+		final Enhancer enhancer = new Enhancer();
+		enhancer.setSuperclass(target.getClass());
+		enhancer.setCallback(new CglibInterceptor(target, aspect));
+		return (T) enhancer.create();
+	}
+
+}
