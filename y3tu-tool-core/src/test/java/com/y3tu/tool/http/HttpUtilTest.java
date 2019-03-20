bd21d26f6d29918cfcfb297fb73b9d70de04e7c5
y3tu <14643092@qq.com>
2019-03-20 16:47:08 +0800

    update

--- y3tu-tool-core/src/test/java/com/y3tu/tool/http/HttpUtilTest.java
+++ y3tu-tool-core/src/test/java/com/y3tu/tool/http/HttpUtilTest.java
@@ -1,16 +1,17 @@
 package com.y3tu.tool.http;
 
 import com.y3tu.tool.http.callback.FileCallBack;
+import org.junit.Test;
 
 /**
- * 下载文件测试
- *
  * @author y3tu
- * @date 2018/10/30
+ * @date 2019-03-20
  */
-public class Test {
+public class HttpUtilTest {
 
-    public static void main(String args[]) {
+    @Test
+    public void get(){
         HttpUtil.get("https://ws1.sinaimg.cn/large/0065oQSqgy1fwgzx8n1syj30sg15h7ew.jpg", new FileCallBack("/Users/yxy/work/", "meizi.jpg"));
     }
+
 }
