86de277e2db0e5cc0e9582468874b08d0316e960
y3tu <14643092@qq.com>
2019-05-27 16:44:02 +0800

    update

--- y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/TreeUtil.java
+++ y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/TreeUtil.java
@@ -1,7 +1,6 @@
 package com.y3tu.tool.core.util;
 
-
-import com.y3tu.tool.core.pojo.Tree;
+import com.y3tu.tool.core.pojo.TreeNode;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -10,24 +9,42 @@ import java.util.Map;
 
 /**
  * 树节点工具类
+ * <p>
+ * <p>
+ * 要使用此工具类，需要用TreeNode构建出List<TreeNode<T>>对象
+ *
+ * eg:
+ *
+ * List<TreeNode<Department>> treeNodeList = list.stream().map(department -> {
+ * TreeNode<Department> treeNode = new TreeNode<>(department.getId(), department.getName(), department.getParentId(), department);
+ * return treeNode;
+ * }).collect(Collectors.toList());
+ *
+ * TreeUtil.buildList(treeNodeList, "0"));
  *
  * @author y3tu
- * @date 2018/7/2
  */
 public class TreeUtil {
 
-    public static <T> Tree<T> build(List<Tree<T>> nodes) {
+    /**
+     * 构建树结构
+     *
+     * @param nodes
+     * @param <T>
+     * @return
+     */
+    public static <T> TreeNode<T> build(List<TreeNode<T>> nodes) {
         if (nodes == null) {
             return null;
         }
-        List<Tree<T>> topNodes = new ArrayList<>();
-        for (Tree<T> children : nodes) {
+        List<TreeNode<T>> topNodes = new ArrayList<>();
+        for (TreeNode<T> children : nodes) {
             String pid = children.getParentId();
             if (pid == null || "0".equals(pid)) {
                 topNodes.add(children);
                 continue;
             }
-            for (Tree<T> parent : nodes) {
+            for (TreeNode<T> parent : nodes) {
                 String id = parent.getId();
                 if (id != null && id.equals(pid)) {
                     parent.getChildren().add(children);
@@ -39,32 +56,40 @@ public class TreeUtil {
 
         }
 
-        Tree<T> root = new Tree<>();
+        TreeNode<T> root = new TreeNode<>();
         root.setId("0");
         root.setParentId("");
         root.setHasParent(false);
         root.setHasChildren(true);
         root.setChecked(true);
         root.setChildren(topNodes);
-        root.setText("根节点");
+        root.setName("根节点");
         Map<String, Object> state = new HashMap<>(16);
         state.put("opened", true);
         root.setState(state);
         return root;
     }
 
-    public static <T> List<Tree<T>> buildList(List<Tree<T>> nodes, String idParam) {
+    /**
+     * 构建树结构
+     *
+     * @param nodes      被构建的数据
+     * @param rootNodeId 根节点id
+     * @param <T>
+     * @return
+     */
+    public static <T> List<TreeNode<T>> buildList(List<TreeNode<T>> nodes, String rootNodeId) {
         if (nodes == null) {
             return null;
         }
-        List<Tree<T>> topNodes = new ArrayList<>();
-        for (Tree<T> children : nodes) {
+        List<TreeNode<T>> topNodes = new ArrayList<>();
+        for (TreeNode<T> children : nodes) {
             String pid = children.getParentId();
-            if (pid == null || idParam.equals(pid)) {
+            if (pid == null || rootNodeId.equals(pid)) {
                 topNodes.add(children);
                 continue;
             }
-            for (Tree<T> parent : nodes) {
+            for (TreeNode<T> parent : nodes) {
                 String id = parent.getId();
                 if (id != null && id.equals(pid)) {
                     parent.getChildren().add(children);
@@ -76,4 +101,5 @@ public class TreeUtil {
         }
         return topNodes;
     }
+
 }
