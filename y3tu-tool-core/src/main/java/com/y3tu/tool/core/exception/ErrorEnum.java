3bc95b57d30ba1d6123e8025301bf334370460bb
y3tu <14643092@qq.com>
2020-01-03 19:31:11 +0800

    update

--- y3tu-tool-core/src/main/java/com/y3tu/tool/core/exception/ErrorEnum.java
+++ y3tu-tool-core/src/main/java/com/y3tu/tool/core/exception/ErrorEnum.java
@@ -10,27 +10,27 @@ public enum ErrorEnum implements IError {
     /**
      * 系统内部错误
      */
-    SYSTEM_INTERNAL_ERROR("SYS-ERROR-001", "系统内部错误"),
+    SYSTEM_INTERNAL_ERROR("SYSTEM_ERROR", "系统内部错误"),
     /**
      * 服务调用异常
      */
-    SERVICE_CALL_ERROR("SYS-ERROR-002", "服务调用异常"),
+    SERVICE_CALL_ERROR("SERVICE_CALL_ERROR", "服务调用异常"),
     /**
      * 未发现服务
      */
-    SERVICE_NOT_FOUND("SYS-ERROR-003", "未发现服务"),
+    SERVICE_NOT_FOUND("SERVICE_NOT_FOUND", "未发现服务"),
     /**
      * 服务运行SQLException异常
      */
-    SQL_EXCEPTION("SYS-ERROR-004", "数据库异常"),
+    SQL_EXCEPTION("SQL_EXCEPTION", "数据库异常"),
     /**
      * 系统繁忙,请稍候再试
      */
-    SYSTEM_BUSY("SYS-ERROR-005", "系统繁忙,请稍候再试"),
+    SYSTEM_BUSY("SYSTEM_BUSY_EXCEPTION", "系统繁忙,请稍候再试"),
     /**
      * Tool工具异常
      */
-    UTIL_EXCEPTION("SYS-ERROR-006", "工具异常");
+    UTIL_EXCEPTION("TOOL_EXCEPTION", "工具异常");
 
 
     String code;
