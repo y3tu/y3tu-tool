34b540affad8aa9ae5f3870d7751ba2764eb178f
y3tu <14643092@qq.com>
2020-01-14 22:30:08 +0800

    update

--- y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/pojo/PageInfo.java
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/pojo/PageInfo.java
@@ -3,8 +3,6 @@ package com.y3tu.tool.web.base.pojo;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * 分页实体
@@ -17,11 +15,6 @@ public class PageInfo<T> extends Page<T> {
     /**
      * 查询条件参数实体
      */
-    T paramEntity;
-
-    /**
-     * 查询条件参数Map
-     */
-    Map<String, Object> paramMap = new HashMap<>();
+    T entity;
 
 }
