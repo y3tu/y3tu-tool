372bb391c067db2d061433b4321d63c7033b898e
y3tu <14643092@qq.com>
2020-01-16 12:32:29 +0800

    update

--- y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/pojo/PageInfo.java
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/base/pojo/PageInfo.java
@@ -3,6 +3,8 @@ package com.y3tu.tool.web.base.pojo;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.Data;
 
+import java.util.Map;
+
 
 /**
  * 分页实体
@@ -16,5 +18,10 @@ public class PageInfo<T> extends Page<T> {
      * 查询条件参数实体
      */
     T entity;
+
+    /**
+     * 查询条件参数
+     */
+    Map params;
 
 }
