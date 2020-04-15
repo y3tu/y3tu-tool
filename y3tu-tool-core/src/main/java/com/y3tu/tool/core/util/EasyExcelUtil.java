d4bf685fa9ff92175abdf98911acc302900c9e6d
y3tu <14643092@qq.com>
2020-04-16 00:04:59 +0800

    update

---
+++ y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/EasyExcelUtil.java
@@ -0,0 +1,15 @@
+package com.y3tu.tool.core.util;
+
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Excel工具类 基于阿里的easyExcel
+ * 基于 https://github.com/alibaba/easyexcel
+ * 更多用法参考 https://www.yuque.com/easyexcel/doc/easyexcel
+ */
+public class EasyExcelUtil {
+
+    public void down(HttpServletResponse response){
+
+    }
+}
