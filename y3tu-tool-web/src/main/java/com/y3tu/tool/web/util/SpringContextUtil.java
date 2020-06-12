9f9e1deb54fac54343d933e5b04ab9ad5336765f
y3tu <14643092@qq.com>
2020-06-12 14:06:17 +0800

    update

--- y3tu-tool-web/src/main/java/com/y3tu/tool/web/util/SpringContextUtil.java
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/util/SpringContextUtil.java
@@ -4,10 +4,12 @@ import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.stereotype.Component;
 
 /**
  * @author y3tu
  */
+@Component
 public class SpringContextUtil implements ApplicationContextAware {
 
     /**
