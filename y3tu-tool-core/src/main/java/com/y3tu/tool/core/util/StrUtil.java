43247130d6c6e7930afdcd9d6c843999dea9010f
y3tu <14643092@qq.com>
2020-07-22 23:07:45 +0800

    update

--- y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/StrUtil.java
+++ y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/StrUtil.java
@@ -8,4 +8,32 @@ package com.y3tu.tool.core.util;
  * @see cn.hutool.core.util.StrUtil
  */
 public class StrUtil extends cn.hutool.core.util.StrUtil {
+
+    /**
+     * 去掉字符串中的空白字符
+     *
+     * @param str
+     * @return
+     */
+    public static String deleteWhitespace(String str) {
+        if (isEmpty(str)) {
+            return str;
+        } else {
+            int sz = str.length();
+            char[] chs = new char[sz];
+            int count = 0;
+
+            for(int i = 0; i < sz; ++i) {
+                if (!Character.isWhitespace(str.charAt(i))) {
+                    chs[count++] = str.charAt(i);
+                }
+            }
+
+            if (count == sz) {
+                return str;
+            } else {
+                return new String(chs, 0, count);
+            }
+        }
+    }
 }
---
+++ y3tu-tool-core/src/main/java/com/y3tu/tool/core/util/ThreadUtil.java
@@ -0,0 +1,9 @@
+package com.y3tu.tool.core.util;
+
+/**
+ * 线程处理工具类
+ *
+ * @see cn.hutool.core.thread.ThreadUtil
+ */
+public class ThreadUtil extends cn.hutool.core.thread.ThreadUtil {
+}
