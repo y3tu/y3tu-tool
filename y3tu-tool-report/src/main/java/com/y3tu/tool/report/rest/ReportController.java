ed5b848c146ae14ce99288763f5dc9ad2b8fc1bb
y3tu <14643092@qq.com>
2020-11-26 01:17:03 +0800

    update

--- y3tu-tool-report/src/main/java/com/y3tu/tool/report/rest/ReportController.java
+++ y3tu-tool-report/src/main/java/com/y3tu/tool/report/rest/ReportController.java
@@ -1,6 +1,7 @@
 package com.y3tu.tool.report.rest;
 
 import com.y3tu.tool.core.pojo.R;
+import com.y3tu.tool.report.repository.ReportRepository;
 import com.y3tu.tool.report.service.ReportService;
 import com.y3tu.tool.web.sql.SqlUtil;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,9 +20,12 @@ public class ReportController {
     @Autowired
     ReportService reportService;
 
+    @Autowired
+    ReportRepository reportRepository;
+
     @GetMapping("/test")
     public R test() {
-        int count = SqlUtil.count("select * from report", "dataSource");
-        return R.success(count);
+        int count = SqlUtil.count("select * from report", "support");
+        return R.success(reportRepository.findById(1));
     }
 }
--- y3tu-tool-web/src/main/java/com/y3tu/tool/web/runner/StartedUpRunner.java
+++ y3tu-tool-web/src/main/java/com/y3tu/tool/web/runner/StartedUpRunner.java
@@ -1,5 +1,6 @@
 package com.y3tu.tool.web.runner;
 
+import cn.hutool.core.util.ReflectUtil;
 import com.y3tu.tool.web.sql.JdbcTemplateUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
 import java.time.LocalDateTime;
+import java.util.Map;
 
 /**
  * 启动完成后提示和一些业务初始化操作
@@ -32,17 +34,29 @@ public class StartedUpRunner implements ApplicationRunner {
             log.info(" / \\ / \\ / \\ / \\ / \\ / \\ / \\ / \\");
             log.info("( c | o | m | p | l | e | t | e )");
             log.info(" \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/ \\_/");
-            log.info("启动完毕，时间：{}",LocalDateTime.now());
+            log.info("启动完毕，时间：{}", LocalDateTime.now());
         }
 
 
         //获取spring容器中所有的数据源bean
         String[] beanNames = context.getBeanNamesForType(DataSource.class);
         for (String name : beanNames) {
-            log.info(String.format("检查到数据源:%s,放入JdbcTemplateUtil工具类中", name));
             DataSource dataSource = context.getBean(name, DataSource.class);
-            JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-            JdbcTemplateUtil.setJdbcTemplate(name, jdbcTemplate);
+            //dataSourceMap是dynamic-datasource多数据源
+            Map dataSourceMap = (Map) ReflectUtil.getFieldValue(dataSource, "dataSourceMap");
+            if (dataSourceMap != null && dataSourceMap.keySet().size() > 0) {
+                for (Object key : dataSourceMap.keySet()) {
+                    JdbcTemplate jdbcTemplate = new JdbcTemplate((DataSource) dataSourceMap.get(key));
+                    JdbcTemplateUtil.setJdbcTemplate(key.toString(), jdbcTemplate);
+                    log.info(String.format("检查到数据源:%s,放入JdbcTemplateUtil工具类中", key.toString()));
+                }
+            } else {
+                JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+                JdbcTemplateUtil.setJdbcTemplate(name, jdbcTemplate);
+                log.info(String.format("检查到数据源:%s,放入JdbcTemplateUtil工具类中", name));
+            }
+            //设置默认
+            JdbcTemplateUtil.setDefaultJdbcTemplate(new JdbcTemplate(dataSource));
         }
 
     }
