cfbe07f0934b92e7881ab1201a0b7a8a9e3acb9e
y3tu <14643092@qq.com>
2020-12-07 16:51:14 +0800

    update & fix bug

--- y3tu-tool-ui/src/main/webapp/src/store/getters.js
+++
@@ -1,7 +0,0 @@
-const getters = {
-  sidebar: state => state.app.sidebar,
-  device: state => state.app.device,
-  token: state => state.user.token,
-  name: state => state.user.name,
-}
-export default getters
