b11e40700cddee870f3a6a2685783d84b0ee19b2
y3tu <14643092@qq.com>
2020-12-07 21:48:26 +0800

    update & fix bug

--- y3tu-tool-ui/src/main/webapp/src/main.js
+++ y3tu-tool-ui/src/main/webapp/src/main.js
@@ -1,4 +1,8 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {createApp} from 'vue'
+import {ElButton, ElSelect} from 'element-plus';
+import App from './App.vue';
 
-createApp(App).mount('#app')
+const app = createApp(App);
+app.use(ElButton);
+app.use(ElSelect);
+app.mount('#app');
