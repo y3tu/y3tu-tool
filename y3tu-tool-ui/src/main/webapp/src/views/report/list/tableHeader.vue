f0a99e7d90cde08a798b60ffdd6a8ec20a5d098b
y3tu <14643092@qq.com>
2021-01-26 17:24:39 +0800

    update & fix bug

--- y3tu-tool-ui/src/main/webapp/src/views/report/list/tableHeader.vue
+++ y3tu-tool-ui/src/main/webapp/src/views/report/list/tableHeader.vue
@@ -1,53 +1,70 @@
 <template>
-    <div :key="editorKey">
-        <code-editor :value="value"
-                     mode="application/json"
-                     height="300px"
-                     @change="valueChange"/>
-    </div>
+  <div :key="editorKey">
+    <code-editor :value="value"
+                 mode="application/json"
+                 height="300px"
+                 @change="valueChange"/>
+  </div>
 
-    <el-tree
-            :data="jsonData"
-            node-key="field"
-            default-expand-all
-            @node-drag-end="handleDragEnd"
-            draggable>
-    </el-tree>
+  <el-tree
+      v-show="!jsonError"
+      :data="jsonData"
+      node-key="field"
+      default-expand-all
+      @node-drag-end="handleDragEnd"
+      draggable>
+  </el-tree>
+
+  <el-alert
+      v-show="jsonError"
+      title="表头Json数据错误,请检查!"
+      type="error">
+  </el-alert>
 
 </template>
 
 <script>
-    import CodeEditor from '@/components/CodeEditor'
+/* eslint-disable */
+import CodeEditor from '@/components/CodeEditor'
 
-    export default {
-        name: 'tableHeader',
-        components: {CodeEditor},
-        props: {
-            value: {
-                type: String,
-                default: ""
-            }
-        },
-        emits:["change"],
-        computed: {
-            jsonData() {
-                return JSON.parse(this.value);
-            }
-        },
-        data() {
-            return {
-                editorKey: 0,
-            };
-        },
-        methods: {
-            handleDragEnd() {
-                let value = JSON.stringify(this.jsonData, null, 2);
-                this.$emit('change', value)
-                this.editorKey = this.editorKey + 1;
-            },
-            valueChange(value) {
-                this.$emit('change', value)
-            }
-        }
+export default {
+  name: 'tableHeader',
+  components: {CodeEditor},
+  props: {
+    value: {
+      type: String,
+      default: ""
+    }
+  },
+  emits: ["change"],
+  computed: {
+    jsonData() {
+      let result = new Object();
+      try {
+        result = JSON.parse(this.value);
+      } catch (e) {
+        this.jsonError = true
+        return
+      }
+      this.jsonError = false
+      return result
+    }
+  },
+  data() {
+    return {
+      editorKey: 0,
+      jsonError: false
     };
+  },
+  methods: {
+    handleDragEnd() {
+      let value = JSON.stringify(this.jsonData, null, 2);
+      this.$emit('change', value)
+      this.editorKey = this.editorKey + 1;
+    },
+    valueChange(value) {
+      this.$emit('change', value)
+    }
+  }
+};
 </script>
