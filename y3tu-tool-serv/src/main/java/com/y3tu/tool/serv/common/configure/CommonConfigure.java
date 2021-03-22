130680699dbcfac9641c7fb5414f8320522465c0
y3tu <14643092@qq.com>
2021-03-22 10:57:23 +0800

    update & fix bug

--- y3tu-tool-serv/src/main/java/com/y3tu/tool/serv/common/configure/CommonConfigure.java
+++ y3tu-tool-serv/src/main/java/com/y3tu/tool/serv/common/configure/CommonConfigure.java
@@ -1,8 +1,6 @@
 package com.y3tu.tool.serv.common.configure;
 
-import com.y3tu.tool.serv.report.configure.ReportProperties;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
